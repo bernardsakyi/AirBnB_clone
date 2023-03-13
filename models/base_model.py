0fd66d4c80ff2813f14337043f5d71b4afb09514
bernardsakyi <sakyibernard77@gmail.com>
2023-03-13 18:35:51 +0000

    Redo

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,7 @@ class BaseModel():
 	This class defines all common methods and attributes for other classes
 	"""
 	
-	def __init__(self, *args, **kwargs):
-		if len(kwargs) > 0:
-			for key, value in kwargs.items():
-				if key != '__class__':
-					if key == 'created_at' or key == 'updated_at':
-						value = datetime.fromisoformat(value)
-					setattr(self, key, value)
-					
-		else:
+	def __init__(self, created_at, updated_at, id=None):
 			self.id = str(uuid.uuid4())
 			self.created_at = datetime.now()
 			self.updated_at = datetime.now()
